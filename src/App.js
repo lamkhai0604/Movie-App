5ed0342fe3447ed3d6c52987ec8b4b6fca4f9fa3
your_name <lamkhai0604@gmail.com>
2020-03-23 14:56:15 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,153 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+// import logo from "./logo.svg";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Navbar, NavDropdown, Form, Nav, FormControl, Button } from 'react-bootstrap'
+import Movie from "./components/Movie"
+import ReactModal from 'react-modal';
+import InputRange from 'react-input-range';
+import 'react-input-range/lib/css/index.css'
+import Pagination from "react-js-pagination";
+
+let apiKey = process.env.REACT_APP_APIKEY
+let keyword = ''
+let movieList = []
 
 function App() {
+
+  let [movies, setMovies] = useState(null);
+  let [modal, setModal] = useState(false);
+  let [trailer, setTrailer] = useState('')
+  let [rate, setRate] = useState[0];
+  let [page, setPage] = useState[1];
+  let [totalResult, setTotalResult] = useState(0);
+
+  let CurrentPlaying = async () => {
+    let url = `https://api.themoviedb.org/3/movie/now_playing?api_key=${apiKey}&language=en-US&page=1`
+    let data = await fetch(url)
+    let dataResult = await data.json();
+    setTotalResult(dataResult.total_results);
+    movie_List = dataResult.results;
+    setMovies(dataResult.results);
+  };
+
+  let playmore = async () => {
+    const url = `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&page=1&sort_by=popularity.desc`
+    let data = await fetch(url)
+    let dataResult = await data.json();
+    famousMovieList = dataResult.results;
+  }
+
+  useEffect(CurrentPlaying, [], []);
+  if (movies == null) {
+    return <div>loading the movie</div>
+  }
+
+  let searchByKeyword = (e) => {
+    keyword = e.target.value;
+    if (keyword === '') {
+      setMovies(movieList)
+    } else {
+      setMovies(movies.filter((movie) => movie.title.toLowerCase().includes(keyword.toLowerCase())));
+    }
+  }
+
+  let sortByPopularity = () => {
+    let sortedMovie = movies.sort((a, b) => a.popularity - b.popularity);
+    setMovies([]);
+    setMovies(sortedMovie);
+  }
+
+  let openModal = async (movie_id) => {
+    let url = `https://api.themoviedb.org/3/movie/${movie_id}/videos?api_key=${apiKey}&language=en-US`
+    let data = await fetch(url);
+    let resultData = await data.json();
+    console.log(resultData.results[0].key)
+    setTrailer(resultData.results[0].key)
+    setModal(!modal)
+  }
+
+  let searchByRate = (value) => {
+    setRate(value);
+    let filteredData = movie_List.filter((movie) => movie.vote_average >= value)
+    setMovies(filteredData)
+  }
+
+  let handlePageChange = async pageNumber => {
+    setPage(pageNumber);
+    let url = `https://api.themoviedb.org/3/movie/now_playing?api_key=${apiKey}&language=en-US&page=1`
+    let data = await fetch(url)
+    let dataResult = await data.json();
+    setMovies(dataResult.results);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div>
+      <Navbar bg="light" expand="lg">
+        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Link</Nav.Link>
+            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+          <Form inline>
+            <FormControl type="text" placeholder="Search" className="mr-sm-2" onChange={(e) => searchByKeyword()} />
+            <Button onClick={() => sortByPopularity()} variant="outline-success">Search</Button>
+          </Form>
+        </Navbar.Collapse>
+      </Navbar>
+      <InputRange
+        maxValue={20}
+        minValue={0}
+        value={rate}
+        onChange={value => searchByRate(value)} />
+      <Movie movieList={movies} openModal={openModal} />
+
+      <ReactModal isOpen={modal}
+        style={{
+          overlay: {
+            backgroundColor: "rgba(0, 0, 0, 0)",
+            top: '10%',
+            left: '10%',
+            right: '10%',
+            bottom: '10%',
+            marginRight: '-10%',
+          },
+          content: {
+            top: '10%',
+            left: '10%',
+            right: '10%',
+            bottom: '10%',
+            marginRight: '0%',
+            backgroundColor: "rgba(0, 0, 0, 0)",
+            border: "none",
+          }
+        }}
+        onRequestClose={() => setModal(false)}>
+
+        <Youtube video={trailer} autoplay className="video" />
+
+      </ReactModal>
+      <Pagination
+        activePage={page}
+        itemsCountPerPage={20}
+        totalItemsCount={totalResult}
+        pageRangeDisplayed={5}
+        onChange={handlePageChange.bind(this)}
+        itemClass="page-item"
+        linkClass="page-link"
+      />
     </div>
+
   );
 }
 
---
+++ src/components/Movie.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import { Card,ListGroup,ListGroupItem } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+
+export default function Movie(props) {
+
+     let htmlMovie = props.movieList.map((movie)=>{
+         return (
+            <Card  className="col-md-4"style={{ width: "18rem" }}>
+            <Card.Img variant="top" src={`https://image.tmdb.org/t/p/w600_and_h900_bestv2/${movie.poster_path}`} />
+            <Card.Body>
+        <Card.Title>{movie.title}</Card.Title>
+              <Card.Text>
+                Some quick example text to build on the card title and make up the
+                bulk of the card's content.
+              </Card.Text>
+            </Card.Body>
+            <ListGroup className="list-group-flush">
+              <ListGroupItem>Cras justo odio</ListGroupItem>
+              <ListGroupItem>Dapibus ac facilisis in</ListGroupItem>
+              <ListGroupItem>Vestibulum at eros</ListGroupItem>
+            </ListGroup>
+            <Card.Body>
+              <Card.Link onClick={()=>props.openModal(movie.id)}>Card Link</Card.Link>
+              <Card.Link href="#">Another Link</Card.Link>
+            </Card.Body>
+          </Card>
+         )
+     })
+    return (
+        <div>
+            {htmlMovie}
+        </div>
+    )
+}
